e8d3a552c21a891cdb86a0cc404eddc4af1708ef
Ben Manning <bmanning@me.com>
2026-03-10 16:37:16 -0400

    first wave of mobile navbar, html, and minimal js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log('this is connected');
+const toggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+toggle.addEventListener('click', () => {
+  const isOpen = navLinks.classList.toggle('is-open');
+  toggle.setAttribute('aria-expanded', isOpen);
+});
